63b75e9b995bb5694e0495809cbc73b84e0fc2bc
Kassio Kennedy <95866074+KaessSantos@users.noreply.github.com>
2023-04-04 18:21:53 -0300

    prevent

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 let btn = document.querySelector("#switch button");
 
-btn.addEventListener("click", function (item) {
+btn.addEventListener("click", function (item){
+  item.preventDefault();
   let html = document.documentElement;
   let img = document.querySelector(".avatar img");
   html.classList.toggle("light");
